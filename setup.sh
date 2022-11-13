5b988753fde4fe5fd868ab6346e7705c7246a8c6
Juan Pablo <juampigarcia2014@gmail.com>
2022-11-12 22:12:35 -0300

    Load Enviroment

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Some instructions for setup an enviroment on python:
+# 1. Create an virtual enviroment: 
+python3 -m virtualenv venv
+
+# 2. Activate the virtual enviroment:
+# source venv/bin/activate or . venv/bin/activate
+. venv/bin/activate
+
+# 3. Install the requirements:
+pip install -r requirements.txt
